80ca056b2ea6600845b3e58656ea7db202b53a2a
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 17:42:24 -0500

    primer commit

---
+++ microservicio-pagos-credito/src/main/java/nttdata/bootcamp/microservicios/pagos/creditos/MicroservicioPagosCreditoApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.pagos.creditos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPagosCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPagosCreditoApplication.class, args);
+	}
+
+}
---
+++ microservicio-pagos-credito/src/test/java/nttdata/bootcamp/microservicios/pagos/creditos/MicroservicioPagosCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.pagos.creditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPagosCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
